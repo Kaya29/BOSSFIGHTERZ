9616e01984525751a6fe2b5e6e453c709fd830d1
Yusuf Salih <saliht94@gmail.com>
2023-11-27 11:18:29 +0300

    Character Controller
    Move & Jump, 5 farklı skin

---
+++ Assets/Characters/Scripts/CharacterMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed = 12f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] float jumpHeight = 3f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = .4f;
+    [SerializeField] LayerMask groundMask;
+    bool isGrounded;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (controller == null)
+        {
+            controller = GetComponent<CharacterController>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
